f68e7c2474948f69bfbde81b68fd4f284eda7aff
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2022-07-19 21:59:36 -0300

    Cadastro, listagem e exclusão de categorias

--- js/categorias.js
+++ js/categorias.js
@@ -2,24 +2,76 @@ const TABELA_CATEGORIAS = document.getElementById('tabela-categorias');
 const MODAL_CONTEUDO = document.getElementById('modal-conteudo');
 
 function abrirImagem(nome) {
-    MODAL_CONTEUDO.innerHTML = `<img src="${nome}" width="100%">`
+    MODAL_CONTEUDO.innerHTML = `<img src="${nome}" width="100%">`;
 }
 
-for (let id = 0; id <= 4; id++) {
-    TABELA_CATEGORIAS.innerHTML += `
-        <tr>
-            <td>${id}</td>
-            <td>Nome ${id}</td>
-            <td>Descrição ${id}</td>
-            <td>
-                <a onclick="abrirImagem('img/carro${id}.jpg')" href="#" data-bs-toggle="modal" data-bs-target="#modal-foto">
-                    <img src="img/carro${id}.jpg" width="100px">
-                </a>
-            </td>
-            <td>
-                <button class="btn btn-warning btn-sm">Editar</button>
-                <button class="btn btn-danger btn-sm">Excluir</button>
-            </td>
-        </tr>
-    `;
+function filtrar() {
+    let expressao = document.getElementById('busca').value.toLowerCase();
+
+    let linhas = TABELA_CATEGORIAS.getElementsByTagName('tr');
+
+    for (let tr in linhas) {
+        let conteudo = linhas[tr].innerHTML.toLowerCase();
+
+        if (true === conteudo.includes(expressao)) {
+            linhas[tr].style.display = '';
+        } else {
+            linhas[tr].style.display = 'none';
+        }
+    }
 }
+
+function inserirCategoria() {
+    event.preventDefault();
+
+    fetch ('http://localhost:9000/categorias', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: document.getElementById('nome').value,
+            descricao: document.getElementById('descricao').value,
+            foto: document.getElementById('foto').value,
+        })
+    });
+
+    alert('Pronto, nova categoria adicionada');
+}
+
+function excluirCategoria(id) {
+    if (false === confirm('Tem certeza?')) {
+        return;
+    }
+    
+    fetch('http://localhost:9000/categorias/'+id, {
+        method: 'DELETE'
+    });
+
+    //recarregar a página
+    alert('Pronto, categoria excluida');
+}
+
+fetch('http://localhost:9000/categorias')
+    .then(resposta => resposta.json())
+    .then(categorias => {
+        categorias.map(cadaCategoria => {
+            TABELA_CATEGORIAS.innerHTML += `
+                <tr>
+                    <td>${cadaCategoria.id}</td>
+                    <td>${cadaCategoria.nome}</td>
+                    <td>${cadaCategoria.descricao}</td>
+                    <td>
+                        <a onclick="abrirImagem('${cadaCategoria.foto}')" href="#" data-bs-toggle="modal" data-bs-target="#modal-foto">
+                            <img src="${cadaCategoria.foto}" width="100px">
+                        </a>
+                    </td>
+                    <td>
+                        <button class="btn btn-warning btn-sm">Editar</button>
+                        <button onclick="excluirCategoria(${cadaCategoria.id})" class="btn btn-danger btn-sm">Excluir</button>
+                    </td>
+                </tr>
+            `;
+        });
+    });
+
